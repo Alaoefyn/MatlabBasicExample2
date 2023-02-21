636465995cd16f79593cc7bd49050d7e090c1bbf
Alaoefyn <121045925+Alaoefyn@users.noreply.github.com>
2023-02-22 00:28:19 +0300

    Update Question3.m

--- Question3.m
+++ Question3.m
@@ -1,11 +1,3 @@
-% Affan Selim Kaya 
-% 200709006
-% Computer Engineering (Eng)
-% Introduction to Programming 
-% CEN103
-% 2020-2021 1st Somestr
-% Project 2 Question 3
-% Asst.Prof. Deniz Dal
 
 % Loop Explanation
 %In this example, we know how many times the loop will work and we can control how many times we flip, so a for loop should be best to use.
@@ -21,8 +13,8 @@ flipResult = round(rand(flipThatTimes,1));
 
 
 % Repeat with coin conversions
-% We need repeat code untýl ýts all possýbýlýtys calculated
-% For an example,ýf we have 2 flips,then we can have 1tails 1heads ,2heads 0tails or 2tails 0heads
+% We need repeat code untÃ½l Ã½ts all possÃ½bÃ½lÃ½tys calculated
+% For an example,Ã½f we have 2 flips,then we can have 1tails 1heads ,2heads 0tails or 2tails 0heads
 
 for j=1:flipThatTimes
     if flipResult(j) == 0
